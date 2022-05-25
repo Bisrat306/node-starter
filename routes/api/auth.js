9b01b1645bacdfd476542c5215e8bd83db53bc41
Bisratemichael Moges <bisrat.moges306@gmail.com>
2022-05-25 10:02:56 +0300

    Added verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 
 // Controllers
-const { register } = require("../../app/controllers/api/AuthController");
+const { register,verify ,login} = require("../../app/controllers/api/AuthController");
 
 // Middleware
 const { registerValidation } = require("../../app/middlewares/auth");
 
 // Routes
 router.post("/register", registerValidation, register);
+router.get("/verify/:token", verify);
+router.post("/login", login);
+router.post("/verify/resend", resendVerification);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ connectToMongoDB();
 
 // Routes
 app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/password", require("./routes/api/forgotPassword"));
 
 // Run the server
 app.listen(5000, () => console.log(`Server running in 5000`));
